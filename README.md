3b926a6c7f339b762049faa76084bf469f8ce39e
jakson de oliveira martins <jaomfive@hotmail.com>
2023-10-26 21:49:15 -0300

    atualizando o Readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dio-trilha-java-basico
 Repositório para exemplos do curso de java Básico pela Dio
+
+# link de Estudos:https://glysns.gitbook.io/java-basico/
+
+# //Declarando uma variavel e Metodos.
+    String meuNome = "Jakson";
+    int anoFabricacao = 2022;
+    boolean verdadeira = false;
+    anoFabricacao = 2018;
+# Metodo
+        Variavel       Metodo       Parametros
+            ↓           ↓               ↓
+String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -1,7 +1,19 @@
 public class MinhaClasse {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         System.out.print("OI Tudo bem, Sejam Bem Vindos !");
+
+        String primeiroNome ="Jakson";
+
+        String segundoNome=" Oliveira Martins ";
+//Todo Metodo tem seus parametros!!!
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+
     }
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat("").concat(segundoNome);
+}
     
 }
